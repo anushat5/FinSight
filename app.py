5d886418fc16fce246e529b0479cc39345a86fbe
Anusha <anushat6633@gmail.com>
2025-06-13 14:58:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from dashboard import show_dashboard
 from forecast import show_forecast
 from assistant import show_ai_assistant
 
-st.set_page_config(page_title="FinSight Pro 💰", layout="wide")
+st.set_page_config(page_title="FinSight 💰", layout="wide")
 
 st.sidebar.title("📂 Navigation")
 page = st.sidebar.radio("Go to", ["Dashboard", "Forecasting", "AI Assistant"])
 
-st.title("💰 FinSight Pro")
+st.title("💰 FinSight")
 st.write("Advanced AI-Powered Financial Analysis & Forecasting Dashboard")
 
 if page == "Dashboard":
